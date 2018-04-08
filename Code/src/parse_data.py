89e0d8f5e63fd05d61b41886684a3fbb625fe38a
Carolyn Atterbury <carolynatterbury@gmail.com>
2018-04-08 11:22:04 -0600

    updated structure of data

---
+++ Code/src/parse_data.py
@@ -0,0 +1,44 @@
+import sys
+import csv
+from os import path
+
+L_nutrients =   [   "Protein", 
+                    "Vitamin C, total ascorbic acid", 
+                    "Vitamin A, IU", 
+                    "Energy", 
+                    "Sodium, Na",
+                    "Fatty acids, total saturated"
+                ]
+
+Nutrients = [
+	{ "name": "protein"},
+	{ "name": "vitaminC"},
+	{ "name": "vitaminA"},
+	{ "name": "calories"},
+	{ "name": "sodium"},
+	{ "name": "saturatedFat"}
+]
+
+L_foods = ['cheese', 'rice', 'beans', 'avocados', 'spinach']
+
+def get_food_nutrients_from_csv(filename, nutrients):
+	file_path = path.relpath("../../Data/"+filename+".csv")
+	with open(file_path, 'rU') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			if not row:
+					continue
+			if row[0] in L_nutrients:
+				item_index = L_nutrients.index(row[0])
+				nutrients[item_index][filename] = row[4]
+	return nutrients 
+
+def get_food_list(foods, nutrients):
+    for food in foods:
+        nutrients = get_food_nutrients_from_csv(food, nutrients)
+    return nutrients
+
+def get_data():
+    return get_food_list(L_foods, Nutrients)
+
+print(get_data())
